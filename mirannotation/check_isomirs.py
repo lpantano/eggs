14d2e08b1e7c9cb31b3f4d42b7fe16d7075b4c78
Lorena <lorena.pantano@gmail.com>
2014-09-04 22:49:28 -0400

    get comparison with isomirs annotation

--- mirannotation/check_isomirs.py
+++ mirannotation/check_isomirs.py
@@ -18,17 +18,33 @@ def _read_miraligner(in_file):
         for line in in_handle:
             cols = line.strip().split("\t")
             logger.debug(line)
-            cref, cquery, pref = _check_pos(cols[1], cols[2], cols[3], cols[4])
-            print "%s %s %s %s %s %s %s %s" % (cols[1], cref, cquery, pref[0], pref[1], int(cols[3])-1, cols[4], cols[-1])
+            ok_mir, ok_add, ok_mut, ok_t5, ok_t3 = _check_pos(cols[1], cols[2], cols[5], cols[6], cols[7], cols[8])
+            print "%s %s %s %s %s %s" % (cols[1], ok_mir, ok_mut, ok_add, ok_t5, ok_t3)
+            logger.debug("res %s %s %s %s %s %s" % (cols[1], ok_mir, ok_mut, ok_add, ok_t5, ok_t3))
 
 
-def _check_pos(name, chr, start, end):
+def _check_pos(name, chr, mut, add, t5, t3):
     """compare name of the seq with position map"""
-    cref = "-".join(name.split("_")[0].lower().split("-")[:3])
-    cquery = "-".join(chr.lower().split("-")[:3])
+    ok_mir, ok_add, ok_mut, ok_t5, ok_t3 = 0, 0, 0, 0, 0
+    cref = name.split("_")[1].lower()
+    cquery = chr.lower()
     logger.debug("%s %s" % (cref, cquery))
-    pref = name.split("_")[1].split(":")
-    return cref, cquery, pref
+    t5_ref, t3_ref = name.split("_")[3].split(":")
+    mut_ref = name.split("_")[4].split(":")[1]
+    add_ref = name.split("_")[5].split(":")[1]
+    if (len(t3)-2 == abs(int(t3_ref))) or (t3 == t3_ref):
+        ok_t3 = True
+    if (len(t5)-2 == abs(int(t5_ref))) or (t5 == t5_ref):
+        ok_t5 = True
+    if mut != "0" and mut_ref != "null":
+        ok_mut = True
+    if mut == "0" and mut_ref == "null":
+        ok_mut = True
+    if len(add)-2 == len(add_ref) or (add == "0" and add_ref == "null"):
+        ok_add = True
+    if cref == cquery:
+        ok_mir = True
+    return ok_mir, ok_add, ok_mut, ok_t5, ok_t3
 
 
 if __name__ == "__main__":
