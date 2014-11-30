547deb3abcea1dfe44d67122de719cae577ab0f1
Lorena <lorena.pantano@gmail.com>
2014-11-29 20:02:52 -0500

    update STAR benchmarking

---
+++ mirannotation/star_genome/map_star.sh
@@ -0,0 +1,1 @@
+STAR --genomeDir /groups/bcbio/bcbio/genomes/Hsapiens/hg19/star --readFilesIn $1 --outFilterMultimapNmax 50 
--- mirannotation/star_genome/parse_bam.py
+++ mirannotation/star_genome/parse_bam.py
@@ -1,12 +1,13 @@
 import os
 from collections import Counter
 
-H = "seq known correct t5 t3 mut add mapped"
+H = "seq known t5 t3 mut add correct mapped amb"
 
 
 def read_bam(fn_name):
     counts = Counter()
     data = {}
+    pairs = {}
     with open(fn_name) as in_handle:
         for line in in_handle:
             cols = line.strip().split("\t")
@@ -14,10 +15,14 @@ def read_bam(fn_name):
             mir_ann = attr[2].split("=")[1]
             is_correct = False
             if cols[14] == "miRNA":
-                ann = " ".join(get_name(cols[3]))
+                ann = get_name(cols[3])
                 if ann[0] == mir_ann:
                     is_correct = True
-                counts[cols[3]] += 1
+                ann = " ".join(ann)
+                pair = (cols[3], mir_ann)
+                if pair not in pairs:
+                    counts[cols[3]] += 1
+                pairs[pair] = 0
                 data[(cols[3], is_correct)] = "%s %s %s %s" % (cols[3], ann, str(is_correct), mir_ann)
     return data, counts
 
@@ -37,7 +42,7 @@ def print_output(data, counts, out_file):
 
 
 def get_name(name):
-    f = name.split("_")
+    f = name.strip().split("_")
     is_add, is_mut, is_5trim, is_3trim = False, False, False, False
     if not f[4].endswith("null"):
         is_mut = True
@@ -53,6 +58,8 @@ def get_name(name):
 def main():
     data, counts = read_bam("sim.20.hsa.bam.anno")
     print_output(data, counts,  "sim.20.hsa.dat")
+    data, counts = read_bam("sim.20.hsa.primary.bam.anno")
+    print_output(data, counts,  "sim.20.hsa.primary.dat")
 
 
 if __name__ == "__main__":
